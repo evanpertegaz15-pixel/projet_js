4feb3d6d5dbf40ff184d8c6ddf6e9a52542e7719
pevan <evan.pertegaz@ynov.com>
2026-03-17 12:16:43 +0100

    feat cipher, fonctions cesar et forceBrute

---
+++ cybershield/js/cipher.js
@@ -0,0 +1,70 @@
+/**
+ * Permet de chiffrer une chaîne de caractères avec un décalage par chiffre.
+ * @param {string} message = le message à chiffrer
+ * @param {number} decalage = le décalage à appliquer pour chiffrer le message
+ */
+const cesar = (message, decalage) => {
+    let resultat = ""
+    const alphabet = /[a-zA-Z]/ // ASCII -> A-Z = 65-90 ; a-z = 97-122
+    const tabMessage = message.split('') // transforme la chaîne en tableau
+    let chaineValide = true
+    if ((decalage >= 1) && (decalage <= 25)) {
+        for (character of tabMessage) {
+            if (!character.match(alphabet)) {
+                chaineValide = false
+                break
+            }
+        }
+        if (chaineValide) {
+            for (character of tabMessage) {
+                resultat += String.fromCharCode(character.charCodeAt(0) + decalage) // utilisation du code ASCII
+            }
+        }
+    }
+    document.querySelector("#message_chiffre").textContent = resultat
+}
+
+/**
+ * Renvoie un tableau des possibilités de déchiffrement par force brute d'une chaîne de caractères.
+ * @param {string} chaine = la chaîne à déchiffrer
+ */
+const forceBrute = (chaine) => {
+    let resultat = []
+    const alphabetMaj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+    const alphabetMin = "abcdefghijklmnopqrstuvwxyz".split('')
+    const tabChaine = chaine.split('')
+    const tabIndex = []
+    for (let i = 0; i < tabChaine.length; i++) {
+        for (let j = 0; j < alphabetMaj.length; j++) {
+            if (tabChaine[i] == alphabetMaj[j] || tabChaine[i] == alphabetMin[j]) {
+                tabIndex.push(j)
+                break
+            }
+        }
+    }
+    for (let k = 1; k < 26; k++) {
+        resultat[k-1] = ""
+        for (let l = 0; l < tabIndex.length; l++) {
+            const index = (tabIndex[l] - (k % 26) + 26) % 26
+            if (alphabetMaj.includes(tabChaine[l])) {
+                resultat[k-1] += alphabetMaj[index]
+            } else if (alphabetMin.includes(tabChaine[l])) {
+                resultat[k-1] += alphabetMin[index]
+            }
+        }
+    }
+    return resultat
+}
+
+/**
+ * Remplit le tableau avec les possibilités de déchiffrement.
+ * @param {Array} tableau = le tableau à remplir
+ */
+const remplirTableau = (tableau) => {}
+
+const message = document.querySelector("#message_a_chiffrer")
+const decalage = document.querySelector("#decalage")
+const bouton = document.querySelector("#bouton_chiffrer")
+const tableauForceBrute = document.querySelector("#tableau_force_brute")
+
+bouton.addEventListener("click", cesar(message, decalage))
